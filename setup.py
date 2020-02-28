d8c153775f440d0a8111b52c96b16ee489be0752
Josh Tesch <jdtesch77@gmail.com>
2020-02-28 16:50:03 -0600

    Add init

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="puppet_master", # Replace with your own username
+    name="puppet_master",
     version="0.0.1",
     author="Josh Tesch",
     author_email="jtesch@overnitecbt.com",
