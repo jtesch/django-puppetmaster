cdd91b9692ec8285d5f9fcb29e1fd523a4dae462
JT <jt@devopsjosh.com>
2020-05-08 16:17:54 -0500

    use html_file as full url to index file, allows for index domain to be different from host

--- puppet_master/puppets/models.py
+++ puppet_master/puppets/models.py
@@ -14,11 +14,14 @@ class Puppet(models.Model):
         help_text="Name of your micro frontend."
     )
     domain_url = models.URLField(
-        help_text="The domain in which the files are hosted."
+        help_text="The public domain in which the files are hosted."
+    )
+    internal_url = models.URLField(
+        help_text="The internal domain"
     )
     html_file = models.CharField(
         max_length=200,
-        help_text="The name of the html file of the built SPA app."
+        help_text="The URL location of the html file of the built SPA app."
     )
     route = models.CharField(
         max_length=255,
